94eba9ae49616c280041826f12b97372b37a2657
yuri <michalwrona4@gmail.com>
2014-08-19 17:04:58 +0200

    Fixed https redirection from exp man

--- reverseProxy/handlers/reverseProxyDirector.go
+++ reverseProxy/handlers/reverseProxyDirector.go
@@ -11,7 +11,7 @@ import (
 func ReverseProxyDirector(context *model.Context) func(*http.Request) {
 	return func(req *http.Request) {
 		fmt.Println()
-		log.Printf("Reverse Proxy : query: %v, from: %v, protocol: %v", req.URL, req.Host, req.Proto)
+		log.Printf("Reverse Proxy : query: %v", req.URL)
 		if host, err := context.ServersList.GetNext(); err != nil {
 			req.URL.Scheme = env.Protocol
 			req.URL.Path = "/error/"
@@ -19,6 +19,7 @@ func ReverseProxyDirector(context *model.Context) func(*http.Request) {
 			log.Printf("Reverse Proxy : error, redirect to %v\n\n", req.URL)
 		} else {
 			req.URL.Scheme = "http"
+			req.Header.Add("X-Forwarded-Proto", env.Protocol)
 			req.URL.Host = host
 			log.Printf("Reverse Proxy : redirect to %v\n\n", req.URL)
 		}
